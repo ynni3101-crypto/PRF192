aa799dccffb9d3c890091eeb8aa3a208d164e79d
pnyn <ynni3101@gmail.com>
2026-01-16 11:47:41 +0700

    ex4

--- slot4/ex2.c
+++ slot4/ex2.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-int main()
-{
+int main(){
 	int ngay,thang,nam;
 	scanf("%d/%d/%d",&ngay,&thang,&nam);
 	printf("%02d/%02d/%d",ngay,thang,nam);
---
+++ slot4/ex4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+	int a,b;
+	char pt;
+	printf("Nhap a,b: ");
+	scanf("%d,%d",&a,&b);
+	printf("Nhap phep toan: ");
+	fflush(stdin);
+	scanf("%c",&pt);
+	if(pt=='/'){
+		if(b==0)
+			printf("Khong the chia cho 0.");	
+		else{
+			float kq=(float)a/b;
+			printf("Ket qua: %d%c%d=%.2f",a,pt,b,kq);}
+	}
+	else
+		printf("Khong phai phep chia.");
+	return 0;
+}
